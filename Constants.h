d663405ac0a65ae99e6b0c139f6bad4006d59cec
lelion <leliondev@icloud.com>
2024-09-08 23:27:24 +1000

    Workable state

--- Constants.h
+++ Constants.h
@@ -7,6 +7,15 @@
 
 #ifndef Constants_h
 #define Constants_h
+#include <string>
+
+using std::string;
+
+namespace Constants
+{
+    const string invalidResponse= "\nThat's an invalid response! Please enter the correct value! \n";
+
+} // namespace constants
 
 
 #endif /* Constants_h */
