8a47cf6b7a0965b1cc7b6913637c4365aada4b81
lelion <leliondev@icloud.com>
2024-09-09 19:56:36 +1000

    removed unit tests

--- tests/testCard.cpp
+++ tests/testCard.cpp
@@ -6,37 +6,37 @@
 #include <vector>
 #include <random>
 
-CardManagement CM;
-Cards card = {
-    Cards::Ranks::Ace,
-    Cards::Suits::clubs,
-};
+// CardManagement CM;
+// Cards card = {
+//     Cards::Ranks::Ace,
+//     Cards::Suits::clubs,
+// };
 
-std::vector<Cards> originalDeck()
-{
-    std::vector<Cards> deck;
-    CM.createDeck();
+// std::vector<Cards> originalDeck()
+// {
+//     std::vector<Cards> deck;
+//     CM.createDeck();
 
-    return deck = CM.getDeck();
-}
+//     return deck = CM.getDeck();
+// }
 
-std::vector<Cards> newDeck()
-{
-    std::vector<Cards> deck;
-    CM.createDeck();
+// std::vector<Cards> newDeck()
+// {
+//     std::vector<Cards> deck;
+//     CM.createDeck();
 
-    return deck = CM.getDeck();
-}
+//     return deck = CM.getDeck();
+// }
 
-int removeCardFromDeck()
-{
-    CM.createDeck();
-    std::vector<Cards> deck = CM.getDeck();
-    int sizeOfDeck = 0;
+// int removeCardFromDeck()
+// {
+//     CM.createDeck();
+//     std::vector<Cards> deck = CM.getDeck();
+//     int sizeOfDeck = 0;
 
-    CM.removeCardFromDeck(card);
-    return sizeOfDeck = deck.size();
-}
+//     CM.removeCardFromDeck(card);
+//     return sizeOfDeck = deck.size();
+// }
 
 // int addNumbers(int a, int b)
 // {
@@ -46,8 +46,8 @@ BOOST_AUTO_TEST_SUITE(CardsTest) // Test suite declaration
 BOOST_AUTO_TEST_CASE(TestDeck)   // Sample test case
 {
     // BOOST_CHECK_EQUAL(5, addNumbers(2, 3));
-    BOOST_CHECK_EQUAL_COLLECTIONS(originalDeck().begin(), originalDeck().end(),
-                                  newDeck().begin(), newDeck().end());
-    BOOST_CHECK_EQUAL(removeCardFromDeck(), 51);
+    // BOOST_CHECK_EQUAL_COLLECTIONS(originalDeck().begin(), originalDeck().end(),
+    //                               newDeck().begin(), newDeck().end());
+    // BOOST_CHECK_EQUAL(removeCardFromDeck(), 51);
 }
 BOOST_AUTO_TEST_SUITE_END()
